6a489852fec8655899a5102f95d44e17212e2170
Darksoul101 <yash9vardhan@gmail.com>
2021-06-22 18:51:30 +0530

    Added Test Cases on getSummary

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-from fetch import info
+import info
 import sys
 
 parser = argparse.ArgumentParser()
--- test/test.py
+++ test/test.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 import unittest
 import sys
-sys.path.append("../fetch/")
-from info import *
+from fetch import info
 
-"""
-TO-DO
-[ ] Write Test Cases 
-"""
+class FetchTest(unittest.TestCase):
+    def getSummary(self):
+        result = info.getSummary('Linux')
+        self.assertEqual(result,"""-------------------------------------------------------Linux--------------------------------------------------------
 
 
-# class InfoTestCase(unittest.TestCase):
-#     def testgetSummary():
-#         result = getSummary('Linux')
-#         self.assertEqual(result)
+Linux (/ˈlinʊks/ (listen) LEEN-uuks or /ˈlɪnʊks/ LIN-uuks) is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. Linux is typically packaged in a Linux distribution.
 
+
+Distributions include the Linux kernel and supporting system software and libraries, many of which are provided by the GNU Project. Many Linux distributions use the word "Linux" in their name, but the Free Software Foundation uses the name "GNU/Linux" to emphasize the importance of GNU software, causing some controversy.""")
