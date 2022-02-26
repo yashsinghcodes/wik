0681f175a29e4cda522fc866a751add8d9e026fd
Yash Singh <32360914+yashsinghcodes@users.noreply.github.com>
2022-02-26 13:06:47 +0530

    Revert "Replace setup.py with flit-flavored pyproject.toml"

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -1,7 +1,3 @@
-"""Fetch is used to get information about anything on the shell using Wikipedia"""
-
-__version__ = "1.0.0"
-
 name = "fetch"
 
 __all__ = ['fetch','info']
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from os import name, path
+from sys import version
+import setuptools
+
+req_pkgs = [
+    'bs4',
+    'requests',
+    'wheel'
+]
+
+
+with open("README.md","r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "yashsinghcodes",
+    version = 1,
+    author = "Yash Singh",
+    author_email = "yash9vardhan@gmail.com",
+    description = "Fetch is use to get information about anything on the shell using Wikipedia",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/yashsinghcodes/fetch",
+    packages = setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fetch=fetch.fetch:arguments',
+        ]
+    },
+    python_requires='>=3.*',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        ],
+    install_requires=req_pkgs,
+    setup_requires=req_pkgs,
+)
