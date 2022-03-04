536875fadc5f55821b2e6ffa8f4bd52e73fd60cb
Yash Singh <32360914+yashsinghcodes@users.noreply.github.com>
2022-03-04 14:05:11 +0530

    fixed few errors

--- wik/wik.py
+++ wik/wik.py
@@ -8,7 +8,7 @@ parser.add_argument("-s","--search",help="Search any topic")
 parser.add_argument("-i","--info",help="Get info on any topic")
 parser.add_argument("-q","--quick",help="Get the summary on any topic")
 parser.add_argument("-l","--lang",help="Get info in your native language (default english)")
-parser.add_argument("-x","--rand",help="Get random Wikipedia article")
+parser.add_argument("-x","--rand",help="Get random Wikipedia article",action='store_true')
 
 a = parser.parse_args()
 
@@ -20,7 +20,7 @@ def arguments():
     if a.quick and a.lang == None:
         info.getSummary(a.quick)
     if a.rand and a.lang == None:
-        info.getRand(a.rand)
+        info.getRand()
     if a.quick and a.lang:
         info.getSummary(a.quick,a.lang)
     if a.info and a.lang:
@@ -28,4 +28,4 @@ def arguments():
     if a.search and a.lang:
         info.searchInfo(a.search,a.lang)
     if a.rand and a.lang:
-        info.getRand(a.rand,a.lang)
+        info.getRand(a.lang)
